46f4c3ce02ddbc5243ba0f98566c2b93f3ff238f
LAUTARO JULIAN FLORES ACUA <lauljfa05@gmail.com>
2023-08-05 16:08:33 -0300

    Anda pero hace toda la cuenta de una, despues tengo que corregirlo. Tambien acomode mejor la dora

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 function operate(a, b, operator) {
+    let result;
     switch (operator) {
         case "+":
-            add(a, b);
+            result = add(a, b);
             break;
         case "-":
-            substract(a, b);
+            result = substract(a, b);
             break;
         case "*":
-            multiply(a, b);
+            result = multiply(a, b);
             break;
         case "/":
-            divide(a, b);
+            result = divide(a, b);
             break;
     }
+    return result;
 }
 
 const add = (a, b) => a + b;
@@ -21,25 +23,58 @@ const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
 const calculatorDisplay = document.getElementById('display');
+const equalsButton = document.getElementById('equals');
 const numberButtons = document.querySelectorAll(".number-button");
+const operatorButtons = document.querySelectorAll(".operator-button");
 
 let display = "";
-let firstNumber = "";
-let secondNumber = "";
+let stringNumber = "";
 let operator = "";
 
 numberButtons.forEach(currentButton => {
     currentButton.addEventListener("click", () => {
         let currentText = currentButton.textContent;
 
-        if (!operator) {
-            firstNumber += currentText;
-        } else {
-            secondNumber += currentText;
+        stringNumber += currentText;
+
+        if (operator) {
+            operator = "";
         }
 
         display += currentText;
         calculatorDisplay.textContent = display;
         console.log(display);
     });
+});
+
+operatorButtons.forEach(currentButton => {
+    currentButton.addEventListener("click", () => {
+        let currentText = currentButton.textContent;
+
+        if (stringNumber && !operator) {
+            operator = currentText;
+            stringNumber += "|" + currentText + "|";
+            display += operator
+            calculatorDisplay.textContent = display;
+        }
+    });
+});
+
+equalsButton.addEventListener("click", () => {
+
+    const baseArray = stringNumber.split("|");
+
+    if (baseArray.length >= 3) {
+        const numberList = baseArray.filter(currentValue => !(["+", "-", "*", "/"].includes(currentValue)));
+        const operatorList = baseArray.filter(currentValue => ["+", "-", "*", "/"].includes(currentValue));
+
+        function reducer(accumulator, currentNumber, index) {
+            const intNumber = parseInt(currentNumber);
+            return index === 0 ? operate(accumulator, intNumber, "+") : operate(accumulator, intNumber, operatorList[index - 1]);
+        }
+
+        display = numberList.reduce(reducer, 0);
+        
+        calculatorDisplay.textContent = display;
+    }
 });
