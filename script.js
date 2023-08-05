f705e62e870015ab1532d07de562ba2039569816
LAUTARO JULIAN FLORES ACUA <lauljfa05@gmail.com>
2023-08-05 10:20:42 -0300

    El display ahora cambia

--- script.js
+++ script.js
@@ -19,3 +19,27 @@ const add = (a, b) => a + b;
 const substract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+const calculatorDisplay = document.getElementById('display');
+const numberButtons = document.querySelectorAll(".number-button");
+
+let display = "";
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+
+numberButtons.forEach(currentButton => {
+    currentButton.addEventListener("click", () => {
+        let currentText = currentButton.textContent;
+
+        if (!operator) {
+            firstNumber += currentText;
+        } else {
+            secondNumber += currentText;
+        }
+
+        display += currentText;
+        calculatorDisplay.textContent = display;
+        console.log(display);
+    });
+});
