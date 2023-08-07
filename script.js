b475c88edf94f8aa2f245621e069a6917952fabb
LAUTARO JULIAN FLORES ACUA <lauljfa05@gmail.com>
2023-08-07 13:22:52 -0300

    Ya anda la dora falta agregar lo de dividir por cero y hacerla mas bonita

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function operate(a, b, operator) {
+    a = parseInt(a);
+    b = parseInt(b);
     let result;
     switch (operator) {
         case "+":
@@ -20,61 +22,48 @@ function operate(a, b, operator) {
 const add = (a, b) => a + b;
 const substract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
+const divide = (a, b) => Math.round(a / b);
 
 const calculatorDisplay = document.getElementById('display');
 const equalsButton = document.getElementById('equals');
+
 const numberButtons = document.querySelectorAll(".number-button");
 const operatorButtons = document.querySelectorAll(".operator-button");
 
+let calculation = [];
 let display = "";
-let stringNumber = "";
-let operator = "";
+let currentNumber = ""; 
 
 numberButtons.forEach(currentButton => {
     currentButton.addEventListener("click", () => {
-        let currentText = currentButton.textContent;
-
-        stringNumber += currentText;
-
-        if (operator) {
-            operator = "";
-        }
-
-        display += currentText;
+        currentNumber += currentButton.textContent;
+        display += currentButton.textContent;
         calculatorDisplay.textContent = display;
-        console.log(display);
     });
 });
 
 operatorButtons.forEach(currentButton => {
     currentButton.addEventListener("click", () => {
-        let currentText = currentButton.textContent;
+        if (currentNumber) {
+            calculation.push(currentNumber, currentButton.textContent);
+            display = calculation.join("");
+            calculatorDisplay.textContent = display;
+            currentNumber = "";
+        }
 
-        if (stringNumber && !operator) {
-            operator = currentText;
-            stringNumber += "|" + currentText + "|";
-            display += operator
+        if (calculation.length > 3) {
+            calculation.splice(0, 3, operate(calculation[0], calculation[2], calculation[1]));
+            display = calculation.join("");
             calculatorDisplay.textContent = display;
         }
     });
 });
 
 equalsButton.addEventListener("click", () => {
-
-    const baseArray = stringNumber.split("|");
-
-    if (baseArray.length >= 3) {
-        const numberList = baseArray.filter(currentValue => !(["+", "-", "*", "/"].includes(currentValue)));
-        const operatorList = baseArray.filter(currentValue => ["+", "-", "*", "/"].includes(currentValue));
-
-        function reducer(accumulator, currentNumber, index) {
-            const intNumber = parseInt(currentNumber);
-            return index === 0 ? operate(accumulator, intNumber, "+") : operate(accumulator, intNumber, operatorList[index - 1]);
-        }
-
-        display = numberList.reduce(reducer, 0);
-        
+    if (calculation.length === 2 && currentNumber) {
+        currentNumber = operate(calculation[0], currentNumber, calculation[1]);
+        calculation.splice(0, 3);
+        display = currentNumber;
         calculatorDisplay.textContent = display;
     }
 });
